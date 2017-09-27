f364db442f2b8bde06cee41594bf7772271c405b
LJIAH <githublin@foxmail.com>
2017-09-27 14:14:02 +0800

    完善推荐栏选项卡,内容更新

--- js/index.js
+++ js/index.js
@@ -21,6 +21,29 @@ window.onload=function() {
             leftnavli[num].className = "cur";
             rightdiv[num].style.display = "block";
         }
+    }
 
+    //推荐栏目Tab选项卡切换
+
+    var reccon = document.getElementsByClassName("recommend_content");
+    reccon[0].style.display = "block";//下标从0开始
+    //找到左侧的li
+    var recnav = document.getElementById("recNav");
+    var recnavli = recnav.getElementsByTagName("li");
+
+    //1.添加点击事件 通过JS 的方式来实现
+    for (var i = 0; i < recnavli.length; i++) {
+        recnavli[i].index = i;
+        recnavli[i].onclick = function () {
+            var num = this.index;
+            //清除冲突的样式
+            for (var j = 0; j < reccon.length; j++) {
+                recnavli[j].className = "";
+                reccon[j].style.display = "none";
+            }
+
+            recnavli[num].className = "cur";
+            reccon[num].style.display = "block";
+        }
     }
 }
