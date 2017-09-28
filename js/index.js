eb55e5e63307cb2ed2cf855c7686b2ebdddf6057
LJIAH <githublin@foxmail.com>
2017-09-28 14:50:38 +0800

    用jquery升级并优化选项卡js代码

--- js/index.js
+++ js/index.js
@@ -1,49 +1,17 @@
 window.onload=function() {
-    //按条件搜索的Tab选项卡切换
-    //找到右侧所有classname为search_nav_right的div
-    var rightdiv = document.getElementsByClassName("search_nav_right");
-    rightdiv[0].style.display = "block";//下标从0开始
-    //找到左侧的li
-    var leftnav = document.getElementById("leftNav");
-    var leftnavli = leftnav.getElementsByTagName("li");
 
-    //1.添加点击事件 通过JS 的方式来实现
-    for (var i = 0; i < leftnavli.length; i++) {
-        leftnavli[i].index = i;
-        leftnavli[i].onclick = function () {
-            var num = this.index;
-            //清除冲突的样式
-            for (var j = 0; j < rightdiv.length; j++) {
-                leftnavli[j].className = "";
-                rightdiv[j].style.display = "none";
-            }
-
-            leftnavli[num].className = "cur";
-            rightdiv[num].style.display = "block";
-        }
+    function tab(aa , bb) {
+        $(bb).eq(0).css("display","block");
+        $(aa).click(function(){
+            $(this).addClass("cur").siblings().removeClass("cur");
+            var index = $(this).index();
+            $(bb).css("display","none");
+            $(bb).eq(index).css("display","block");
+        })
     }
 
-    //推荐栏目Tab选项卡切换
+    $(tab("#leftNav li",".search_nav_right"));
 
-    var reccon = document.getElementsByClassName("recommend_content");
-    reccon[0].style.display = "block";//下标从0开始
-    //找到左侧的li
-    var recnav = document.getElementById("recNav");
-    var recnavli = recnav.getElementsByTagName("li");
+    $(tab("#recNav li",".recommend_content"));
 
-    //1.添加点击事件 通过JS 的方式来实现
-    for (var i = 0; i < recnavli.length; i++) {
-        recnavli[i].index = i;
-        recnavli[i].onclick = function () {
-            var num = this.index;
-            //清除冲突的样式
-            for (var j = 0; j < reccon.length; j++) {
-                recnavli[j].className = "";
-                reccon[j].style.display = "none";
-            }
-
-            recnavli[num].className = "cur";
-            reccon[num].style.display = "block";
-        }
-    }
 }
