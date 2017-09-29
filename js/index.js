70a0cafad4e9206c6e19b48d03b847032e066a51
LJIAH <githublin@foxmail.com>
2017-09-29 19:58:53 +0800

    添加轮播图

--- js/index.js
+++ js/index.js
@@ -14,4 +14,39 @@ window.onload=function() {
 
     $(tab("#recNav li",".recommend_content"));
 
+    var index=0;
+    $(function(){
+        var pics=$(".bg li");
+        var lis=$(".indicator li");
+        function changePic(){
+            pics.eq(index).addClass("current").siblings().removeClass("current");
+            lis.eq(index).addClass("current").siblings().removeClass("current");
+        }
+        lis.click(function(){
+            index=$(this).index();
+            changePic();
+        })
+        $(".left").click(function(){
+            index--;
+            if(index==-1){
+                index=4
+            }
+            changePic();
+        })
+        $(".right").click(function(){
+            index++;
+            if(index==5){
+                index=0;
+            }
+            changePic();
+        })
+        var t = setInterval(function(){$(".right").click()},2000);
+        $("#carousel").mouseover(function () {
+            clearInterval(t);
+        });
+        $("#carousel").mouseout(function () {
+            t = setInterval(function(){$(".right").click()},2000);
+        });
+    })
+
 }
